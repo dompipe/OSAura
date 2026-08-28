7aafd71dd9a407034794ea72ecdb7422119d0aca
dompipe <inland14@live.com>
2026-08-28 05:19:44 -0400

    Add Intel e1000 core driver API

---
+++ kernel/e1000.h
@@ -0,0 +1,16 @@
+#ifndef OSAURA_E1000_H
+#define OSAURA_E1000_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t bytes[6];
+} osaura_mac_address;
+
+int osaura_e1000_init(uint8_t bus, uint8_t device, uint8_t function);
+int osaura_e1000_ready(void);
+const osaura_mac_address *osaura_e1000_mac(void);
+int osaura_e1000_transmit(const void *frame, uint16_t bytes);
+int osaura_e1000_receive(void *frame, uint16_t capacity, uint16_t *bytes_out);
+
+#endif
