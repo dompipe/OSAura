08e81d868bb98ec0119bc2e86525114615a38696
dompipe <inland14@live.com>
2026-08-28 04:53:26 -0400

    Define native USB HID keyboard interface

---
+++ kernel/usb-keyboard.h
@@ -0,0 +1,8 @@
+#ifndef OSAURA_USB_KEYBOARD_H
+#define OSAURA_USB_KEYBOARD_H
+
+int osaura_usb_keyboard_init(void);
+int osaura_usb_keyboard_ready(void);
+void osaura_usb_keyboard_poll(void);
+
+#endif
