6499bc8e106549747a21223b4d745e20bb2b4383
dompipe <inland14@live.com>
2026-08-28 05:02:55 -0400

    Expose USB keyboard admission stage

--- kernel/usb-keyboard.h
+++ kernel/usb-keyboard.h
@@ -1,8 +1,11 @@
 #ifndef OSAURA_USB_KEYBOARD_H
 #define OSAURA_USB_KEYBOARD_H
 
+#include <stdint.h>
+
 int osaura_usb_keyboard_init(void);
 int osaura_usb_keyboard_ready(void);
+uint32_t osaura_usb_keyboard_stage(void);
 void osaura_usb_keyboard_poll(void);
 
 #endif
