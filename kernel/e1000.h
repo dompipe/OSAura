7698e82daf67955072198d7b7a26e52e4ff2a0f9
dompipe <inland14@live.com>
2026-08-28 11:58:59 -0400

    Make network transmit subject-aware

--- kernel/e1000.h
+++ kernel/e1000.h
@@ -10,6 +10,7 @@ typedef struct {
 int osaura_e1000_init(uint8_t bus, uint8_t device, uint8_t function);
 int osaura_e1000_ready(void);
 const osaura_mac_address *osaura_e1000_mac(void);
+int osaura_e1000_transmit_as(uint32_t subject, const void *frame, uint16_t bytes);
 int osaura_e1000_transmit(const void *frame, uint16_t bytes);
 int osaura_e1000_receive(void *frame, uint16_t capacity, uint16_t *bytes_out);
 
