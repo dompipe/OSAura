377ef0a736af570170166ab046843911402a265e
dompipe <inland14@live.com>
2026-08-28 14:38:43 -0400

    Add WSJX64 processor bus adapter contract

---
+++ host/windows/processor-bus-win64.h
@@ -0,0 +1,7 @@
+#ifndef OSAURA_WINDOWS_PROCESSOR_BUS64_H
+#define OSAURA_WINDOWS_PROCESSOR_BUS64_H
+
+/* Bind the shared processor multiplex bus to WSJX64 TASK/JOB64. */
+int osaura_windows_processor_bus64_bind(void);
+
+#endif
