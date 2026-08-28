fdf2b2d45a7c970abf02bba41bec918d814a8779
dompipe <inland14@live.com>
2026-08-28 14:05:39 -0400

    Add Win64 JX11 surface storage backend

---
+++ host/windows/jx11-surface-win64.h
@@ -0,0 +1,7 @@
+#ifndef OSAURA_WINDOWS_JX11_SURFACE_WIN64_H
+#define OSAURA_WINDOWS_JX11_SURFACE_WIN64_H
+
+int osaura_windows_jx11_surface_install(void);
+void osaura_windows_jx11_surface_shutdown(void);
+
+#endif
